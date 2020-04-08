7ed08e3382e0dd1cd81544afbd66e78c5860869b
naveenkanthb <naveenkanth033@gmail.com>
2020-04-08 11:54:47 +0530

    commiting changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import uuid from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 
